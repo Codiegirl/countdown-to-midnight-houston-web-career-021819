fab607634d16ec38f950ee52dc799a19372b0ba7
Ro Flo <iroflood@msn.com>
2019-02-15 16:17:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,5 +10,12 @@ end
 return "HAPPY NEW YEAR!"
 end
 
-countdown_with_sleep(sleep)
-sleep 1 
+
+def countdown_with_sleep(sleep)
+  while sleep > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
